c36365d0cae7fd4690d4067230f40473a76ad26f
BuildTools <unconfigured@null.spigotmc.org>
2021-01-27 11:56:11 -0500

    following user example

---
+++ routes/users.js
@@ -0,0 +1,63 @@
+const userRoutes = (app, fs) => {
+    // variables
+    const dataPath = './data/users.json';
+
+    const readFile = (
+        callback,
+        returnJson = false,
+        filePath = dataPath,
+        encoding = 'utf8'
+    ) => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            callback(returnJson ? JSON.parse(data) : data);
+        });
+    };
+
+    const writeFile = (
+        fileData,
+        callback,
+        filePath = dataPath,
+        encoding = 'utf8'
+    ) => {
+        fs.writeFile(filePath, fileData, encoding, err => {
+            if (err) {
+                throw err;
+            }
+
+            callback();
+        });
+    };
+
+    // READ
+    // Notice how we can make this 'read' operation much more simple now.
+    app.get('/users', (req, res) => {
+        readFile(data => {
+            res.send(data);
+        }, true);
+    });
+
+    // CREATE
+    app.post('/users', (req, res) => {
+        readFile(data => {
+            // Note: this needs to be more robust for production use. 
+            // e.g. use a UUID or some kind of GUID for a unique ID value.
+            const newUserId = Date.now().toString();
+
+            // add the new user
+            data[newUserId] = req.body;
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send('new user added');
+            });
+        }, true);
+    });
+
+
+
+};
+
+module.exports = userRoutes;
